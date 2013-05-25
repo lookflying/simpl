f3f7672e0494ddbba6ee4fc41b108d6f19ed9db7
wanghao <haow1990@gmail.com>
2013-05-26 00:08:40 +0800

    add testcase in SimpleExpression

--- src/test/SimpleExpressions.java
+++ src/test/SimpleExpressions.java
@@ -2,27 +2,38 @@ package test;
 
 import org.junit.Test;
 
+import static test.TestUtil.*;
+
 public class SimpleExpressions {
 
 	@Test
 	public void testSimpleInt() throws Exception {
-		TestUtil.runScript("54$", 54);
-		TestUtil.runScript("1$", 1);
-		TestUtil.runScript("   189689	$", 189689);
+		runScript("54$", 54);
+		runScript("1$", 1);
+		runScript("   189689	$", 189689);
 	}
 
 	@Test
 	public void testSimpleList() throws Exception {
-		TestUtil.runScript("nil$");
-		TestUtil.runScript("5::nil$");
-		TestUtil.runScript("9::4343::nil$");
+		runScript("nil$");
+		runScript("5::nil$");
+		runScript("9::4343::nil$");
+		
+		runScript("head 8::nil$", 8);
+		runScript("head (1=2)::nil$", false);
 	}
 
 	@Test
 	public void testSimplePair() throws Exception {
-		TestUtil.runScript("(1 ,8)$");
-		TestUtil.runScript("(nil, 8::nil)$");
-		TestUtil.runScript("(8::98::988::nil, nil)$");
+		runScript("(1 ,8)$");
+		runScript("(nil, 8::nil)$");
+		runScript("(8::98::988::nil, nil)$");
 	}
-
+	
+	@Test
+	public void testSimpleBoolean() throws Exception {
+		runScript(" 1 = 2 $", false);
+		runScript("1=1$", true);
+		runScript("not(1=2)$", true);
+	}
 }
