a525de0d99e5dfeb76977a19b2c082bedfe12400
wanghao <haow1990@gmail.com>
2013-05-26 00:01:56 +0800

    change junit package structure; add getters to BoolValue and IntValue

--- src/test/SimpleExpressions.java
+++ src/test/SimpleExpressions.java
@@ -6,9 +6,23 @@ public class SimpleExpressions {
 
 	@Test
 	public void testSimpleInt() throws Exception {
-		TestUtil.runScript("54$");
-		TestUtil.runScript("1$");
-		TestUtil.runScript("189689$");
+		TestUtil.runScript("54$", 54);
+		TestUtil.runScript("1$", 1);
+		TestUtil.runScript("   189689	$", 189689);
+	}
+
+	@Test
+	public void testSimpleList() throws Exception {
+		TestUtil.runScript("nil$");
+		TestUtil.runScript("5::nil$");
+		TestUtil.runScript("9::4343::nil$");
+	}
+
+	@Test
+	public void testSimplePair() throws Exception {
+		TestUtil.runScript("(1 ,8)$");
+		TestUtil.runScript("(nil, 8::nil)$");
+		TestUtil.runScript("(8::98::988::nil, nil)$");
 	}
 
 }
--- src/test/TestUtil.java
+++ src/test/TestUtil.java
@@ -2,9 +2,14 @@ package test;
 
 import java.io.ByteArrayInputStream;
 
+import static org.junit.Assert.*;
+
 import parse.Lexer;
 import parse.parser;
 import semantic.Env;
+import syntax.BoolValue;
+import syntax.IntValue;
+import syntax.Value;
 
 public class TestUtil {
 	
@@ -15,6 +20,32 @@ public class TestUtil {
 		System.out.println("Parse result: \n" + p.result.toString());
 		Env env = new Env();
 		System.out.println("Result: " + p.result.execute(env));
+	}
+	
+	public static void runScript(String script, Integer result) throws Exception {
+		System.out.println(">>>>>>>>>>>>>>>>>>>\nRunning: \n" + script);
+		parser p = new parser(new Lexer(new ByteArrayInputStream(script.getBytes())));
+		p.parse();
+		System.out.println("Parse result: \n" + p.result.toString());
+		Env env = new Env();
+		Value v = p.result.execute(env);
+		System.out.println("Result: " + v);
+		if (result == null) {
+			assertTrue(((IntValue)v).isUndef());
+		} else {
+			assertEquals(result.intValue(), ((IntValue)v).getValue());
+		}
+	}
+	
+	public static void runScript(String script, boolean result) throws Exception {
+		System.out.println(">>>>>>>>>>>>>>>>>>>\nRunning: \n" + script);
+		parser p = new parser(new Lexer(new ByteArrayInputStream(script.getBytes())));
+		p.parse();
+		System.out.println("Parse result: \n" + p.result.toString());
+		Env env = new Env();
+		Value v = p.result.execute(env);
+		System.out.println("Result: " + v);
+		assertEquals(result, ((BoolValue)v).getValue());
 	}
 
 }
