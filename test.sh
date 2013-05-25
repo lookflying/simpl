c8ecd0c26a41157ed08d924b96b5f2bc0459719a
lookflying <lookflying@gmail.com>
2013-05-25 13:29:36 +0800

    modified test.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
 function run(){
-	echo "========================"
+	echo "-------------------------"
 	echo "file: $1"
 	file=$1
 	java -cp bin:src/java-cup-11a-runtime.jar run.Main $1 > $tmp
-	if [ $? -eq 0 ]
+	rtval=$?
+	echo "return value = $rtval"
+	if [ $rtval -eq 0 ]
 	then
-		echo "pass"
+	echo "pass"
+		cat $tmp
 	else
 		cat $tmp
 	fi
-	echo "========================"
+	echo "_________________________"
 }
 
 if [ $# -eq 1 ]
 then
-	dir=$1
+	target=$1
 else
-	dir=`pwd`
+	target=`pwd`
 fi
 tmp=`mktemp`
-for file in $dir/*.spl
-do
-	run $file
-	echo
-done
+if [ -d $target ]
+then
+	for file in $target/*.spl
+	do
+		run $file
+		echo
+	done
+elif [ -f $target ]
+then
+	run $target
+fi
 rm $tmp
