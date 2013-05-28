79ba18fdfa64b0b26c2825ade1d9967251579e22
wanghao <haow1990@gmail.com>
2013-05-28 21:39:31 +0800

    supply parser with line and column position, but the line report seems wrong

--- src/run/SimPL.java
+++ src/run/SimPL.java
@@ -3,10 +3,13 @@ package run;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
@@ -14,8 +17,6 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-
-import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 import parse.Lexer;
 import parse.parser;
@@ -42,16 +43,23 @@ public class SimPL {
 			options.addOption("s", "shell", false, "interactive shell");
 			// -f file
 			options.addOption("f", "file", true, "execute file");
+			// -o out
+			options.addOption("o", "out", true, "output file");
 			
 			CommandLineParser parser = new PosixParser();
 			CommandLine cmd = parser.parse(options, args);
 			
+			String outfile = null;
+			if (cmd.hasOption("o")) {
+				outfile = cmd.getOptionValue("o", null);
+			}
+			
 			if (cmd.hasOption("h")) {
 				printUsage();
 			} else if (cmd.hasOption("s")) {
-				interactiveShell();
+				run(null, outfile, true);
 			} else if (cmd.hasOption("f")) {
-				simplScriptFile(cmd.getOptionValue("f"));
+				run(cmd.getOptionValue("f"), outfile, false);
 			} else {
 				printUsage();
 				System.exit(-1);
@@ -60,17 +68,38 @@ public class SimPL {
 			printUsage();
 			System.exit(-1);
 		}
+		System.exit(0);
 	}
 	
 	public static void printUsage() {
 		System.err.println("Usage: SimPL [-s | -f file | -h]");
 	}
 	
-	public static void interactiveShell() {
+	/**
+	 * run SimPL, read from console or file, output to file or console
+	 * @param infile input file, if null supplied, then stdin
+	 * @param outfile output file, if null supplied, then stdout
+	 */
+	public static void run(String infile, String outfile, boolean printPrompt) {
+		Scanner in = null;
+		PrintWriter out = null;
 		Scanner pipereader = null;
 		PrintWriter pipeWriter = null;
-		Scanner stdin = null;
 		try {
+			// create input scanner
+			if (infile == null || infile.isEmpty()) {
+				in = new Scanner(System.in);
+			} else {
+				in = new Scanner(new FileInputStream(infile));
+			}
+			
+			// create output printer
+			if (outfile == null || outfile.isEmpty()) {
+				out = new PrintWriter(System.out);
+			} else {
+				out = new PrintWriter(new FileOutputStream(outfile));
+			}
+			
 			// construct a pipe
 			PipedInputStream pipeinstream = new PipedInputStream(MAX_BUF_SIZE);
 			PipedOutputStream pipeoutstream = new PipedOutputStream(pipeinstream);
@@ -78,92 +107,131 @@ public class SimPL {
 			pipereader.useDelimiter("\\$");
 			pipeWriter = new PrintWriter(pipeoutstream);
 			
-			stdin = new Scanner(System.in);
-			int dollarCount = 0;
 			int bufsize = 0;
-			System.out.print("SimPL> ");
-			while (stdin.hasNextLine()) {
-				// read a line from stdin, delete white characters
-				String line = stdin.nextLine().trim();
+			Queue<Position> scriptStarts = new LinkedList<Position>();
+			scriptStarts.add(Position.ORIGIN_POSITION);
+			int linenum = 0;
+			
+			if (printPrompt) {
+				System.out.print("SimPL> ");
+			}
+			while (in.hasNextLine()) {
+				// read a line from stdin
+				String line = in.nextLine();
+				// if it is only a blank line and nothing in buffer
+				// then just ingore
+				if (line.trim().isEmpty() && bufsize == 0) {
+					if (printPrompt) {
+						System.out.print("SimPL> ");
+					}
+					continue;
+				}
+				
 				// add the total buffer size
 				bufsize += line.length() + 1;
 				// should not be more than 2k, if so, then abort
 				if (bufsize > MAX_BUF_SIZE) {
 					bufsize = 0;
-					dollarCount = 0;
+					// clear script positions
+					if (scriptStarts.size() != 1) {
+						scriptStarts.clear();
+						scriptStarts.add(Position.ORIGIN_POSITION);
+					}
+					// clear the pipe
 					pipeWriter.flush();
 					if (pipeinstream.available() != 0) {
 						pipeinstream.read(dummyBuf, 0, MAX_BUF_SIZE);
 					}
+					// reset linenum
+					linenum = 0;
+					// print error message and continue
 					System.err.println("Error: A script should be less than " + MAX_BUF_SIZE + " characters");
-					System.out.print("SimPL> ");
+					if (printPrompt) {
+						System.out.print("SimPL> ");
+					}
 					continue;
 				}
-				// count occurance of $, the number of expressions
-				dollarCount += countDollar(line);
-				// store the line into the pipe to read
-				pipeWriter.print('\n');
-				pipeWriter.print(line);
 				
+				// count occurance of $, the number of expressions
+//					dollarCount += countDollar(line);
+				++linenum;
+				countDollar(line, linenum, scriptStarts);
+				// store the line into the pipe to read, including a new line
+				pipeWriter.println(line);
 				
 				// a script will be executed only
 				// when the last character of the line is $
 				// if not, continue to read from stdin
-				if (line.endsWith("$") == false) {
+				if (line.trim().endsWith("$") == false) {
 					continue;
 				}
 				
 				// now execute the scripts
 				pipeWriter.flush();
-				while (dollarCount > 0) {
-					String script = pipereader.next().trim();
+				while (scriptStarts.size() > 1) {
+					String script = pipereader.next();
+					Position pos = scriptStarts.remove();
+					
 					// if script is just an empty string, then ingore it
-					if (script.isEmpty()) {
+					if (script.trim().isEmpty()) {
 						continue;
 					}
+					ErrorMessage.init(pos.line, pos.column);
 					runScript(script + "$");
-					--dollarCount;
 				}
 				
-				System.out.print("SimPL> ");
+				// clear script positions
+				scriptStarts.clear();
+				scriptStarts.add(Position.ORIGIN_POSITION);
+				// clear
+				bufsize = 0;
+				// clear the pipe
+				if (pipeinstream.available() != 0) {
+					pipeinstream.read(dummyBuf, 0, MAX_BUF_SIZE);
+				}
+				// reset linenum
+				linenum = 0;
+				// print error message and continue
+				if (printPrompt) {
+					System.out.print("SimPL> ");
+				}
 			}
+		} catch (FileNotFoundException e) {
+			System.err.println("cannot open file: " + e.getMessage());
+			System.exit(-1);
 		} catch (IOException e) {
-			System.err.println("Unexpected error occurred: " + e.getMessage());
+			System.err.println("Unexpected Error");
 			e.printStackTrace();
 			System.exit(-1);
 		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
 			if (pipereader != null) {
 				pipereader.close();
 			}
 			if (pipeWriter != null) {
 				pipeWriter.close();
 			}
-			if (stdin != null) {
-				stdin.close();
-			}
 		}
 	}
 	
-	public static void simplScriptFile(String filename) {
-		System.out.println("simplScriptFile " + filename);
-		try {
-			Scanner file = new Scanner(new FileInputStream(filename));
-			file.useDelimiter("\\$");
-			while (file.hasNext()) {
-				String script = file.next().trim();
-				// if script is just an empty string, then exit
-				if (script.length() == 0) {
-					System.exit(0);
-				} else if (runScript(script + "$") != 0) {
-					System.exit(1);
-				}
-			}
-			
-			file.close();
-		} catch (FileNotFoundException e) {
-			System.err.println("cannot open file: " + e.getMessage());
+	private static class Position {
+		public int line;
+		public int column;
+		
+		public Position(int line, int column) {
+			super();
+			this.line = line;
+			this.column = column;
 		}
+		
+		public static final Position ORIGIN_POSITION = new Position(1, 1);
 	}
+	
 	
 	/**
 	 * run a simpl script
@@ -188,14 +256,12 @@ public class SimPL {
 		return retval;
 	}
 	
-	private static int countDollar(String str) {
-		int count = 0;
+	private static void countDollar(String str, int linenum, Queue<Position> scriptStarts) {
 		for (int i = 0; i < str.length(); ++i) {
 			if (str.charAt(i) == '$') {
-				++count;
+				scriptStarts.add(new Position(linenum, i + 2));
 			}
 		}
-		return count;
 	}
 
 }
